8392251cb6663289d2649ce0510362784c03c0b4
ygarrot <garrot.yoan1@gmail.com>
2022-03-04 14:34:23 +0100

    no more problems with matrix sizes

--- src/multilayerPerceptron.py
+++ src/multilayerPerceptron.py
@@ -1,48 +1,83 @@
 import numpy as np
 import scipy.special
 
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
+def relu(x, d=False):
+    if d:
+        x[x >= 0] = 1
+    else:
+        x[x < 0] = 0
+    return x
+
+def softmax():
+    pass
+
+def sigmoid(z, d=False):
+    s = scipy.special.expit(z)
+    if d:
+        return z * s * (1 - s)
+    return z
 
 class Layer:
-    def __init__(self, number_of_features, activation_units, layer_type):
-        # init_epsilon = [[-sys.float_info.epsilon, sys.float_info.epsilon]]
-        self.weight = (np.random.rand(activation_units, number_of_features))#  @ init_epsilon) - init_epsilon
-        # self.weight = np.random.normal(0.0, pow(number_of_features, -0.5), (activation_units, number_of_features))
-        # self.bias = np.ones(activation_units)
-        self.bias = np.random.rand(activation_units, 1)
-        self.activation_function = scipy.special.expit #sigmoid
+    def __init__(self, number_of_features, activation_units,
+                 layer_type, activation_function=sigmoid):
+        self.weight = np.random.rand(activation_units, number_of_features) * 0.01
+        self.bias = np.zeros((activation_units, 1))
+        self.Activation_function = activation_function
         self.type = layer_type
+        assert(self.weight.shape == (activation_units, number_of_features))
+        assert(self.bias.shape == (activation_units, 1))
 
-    # /!\ Matrix order is very important here
-    def z(self, x):
-        return (self.weight @ x) +  self.bias
+    def linear_forward(self, x):
+        self.z = (self.weight @ x) +  self.bias
+        assert(self.z.shape == (self.weight.shape[0], x.shape[1]))
+        return self.z
 
-    def feedforward(self, x):
-        self.h = self.activation_function(self.z(x))
-        return self.h
+    def linear_backward(self, A_prev, W):
+        W = self.weight
+        dZ = self.Activation_function(A_prev, d=True)
+        m = A_prev.shape[0]
+        dW = np.dot(dZ, self.A_prev.T)
+        db = np.sum(np.matrix(dZ), axis=1)
+        dA_prev = np.dot(W.T, dZ)
+ 
+        assert(dA_prev.shape == self.A_prev.shape)
+        assert(dW.shape == W.shape)
+        # assert (db.shape == b.shape)
+        return dA_prev, self.weight, dW, db
+
+    def update_parameters(self, rate, dW, db):
+        self.weight -= rate * dW
+        self.bias -= rate * db
+
+    def linear_activation_forward(self, A_prev):
+        self.A_prev = A_prev
+        self.A = self.Activation_function(self.linear_forward(A_prev))
+        assert (self.A.shape == (self.weight.shape[0], A_prev.shape[1]))
+        return self.A
 
 class MultilayerPerceptron:
-    def __init__(self, layers, lr=0.1):
+    def __init__(self, layers, lr=0.001):
         self.lr = lr
-        self.layers=layers
+        self.layers = layers
 
-    # def loss(self, p, y):
-    #     return -((y @ np.log(p) + (1 - y) @ np.log(1 - p)) / y.shape[0])
+    def loss(self, p, y):
+        m = -y.shape[0]
+    #     # return -np.squeeze(np.sum((y @ np.log(p)) + ((1 - y) @ np.log(1 - p))) /m)
+        cost = (1./m) * (-np.dot(y,np.log(p).T) - np.dot(1-y, np.log(1-p).T))
+        return np.squeeze(cost)
 
     def forward_propagation(self, x):
-        for i, layer in enumerate(self.layers):
-            x = layer.feedforward(x)
+        for layer in self.layers:
+            x = layer.linear_activation_forward(x)
         return x
 
-    def backward_propagation(self, inputs, targets):
-        fw = self.forward_propagation(inputs)
-        error = targets - fw
-
-        t = [self.layers[0].h, inputs]
-        for i, layer in enumerate(self.layers[::-1]):
-            gradient = error * (layer.h * (1.0 - layer.h))
-            error = layer.weight.T @ error
-            layer.weight += self.lr * (gradient @ t[i].T)
-            layer.bias += (self.lr * gradient)
-    
+    def backward_propagation(self, X, Y):
+        AL = self.forward_propagation(X)
+        Y = Y.reshape(AL.shape)
+        # A_prev = -(np.divide(Y, AL) - np.divide(1 - Y, 1 - AL))
+        # A_prev = AL-Y
+        W = self.layers[-1].weight
+        for i in range(len(self.layers)-1, -1, -1):
+            A_prev, W, dW, db = self.layers[i].linear_backward(A_prev, W)
+            self.layers[i].update_parameters(self.lr, dW, db)
+        return AL
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,7 +1,15 @@
-# import argparse
-# import random
-# import sys
+import argparse
+import numpy as np
 import pandas as pd
+from multilayerPerceptron import *
+import matplotlib.pyplot as plt
+
+def test_train_split(df, ratio):
+    rat = len(df) * ratio
+    msk = np.random.rand(len(df)) < ratio
+    x = df.loc[ : , df.columns != 'Diagnosis']
+    y = df['Diagnosis']
+    return x[msk], x[~msk], y[msk], y[~msk]
 
 categories = [
     'radius',
@@ -22,18 +30,49 @@ stats = [
     'worst'
 ]
 
-concat = ['ID', 'Diagnosis'] + \
-    [' '.join([cat, stat]) for cat in categories for stat in stats ]
-df = pd.read_csv("./resources/data.csv")
-df.columns = concat
-print(df.head())
-df.reset_index(drop=True, inplace=True)
-df.to_csv("./resources/data_p.csv", index=False)
+def add_columns_name(df):
+    concat = ['ID', 'Diagnosis'] + \
+        [' '.join([cat, stat]) for cat in categories for stat in stats ]
+    df.columns = concat
+    df.reset_index(drop=True, inplace=True)
+    df.to_csv("./resources/data_p.csv", index=False)
 
-# def main():
-#     parser = argparse.ArgumentParser()
-#     parser.add_argument("path", type=str, default="./resources/data.csv", help="dataset_train.csv")
-#     args = parser.parse_args()
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("path", type=str, default="./resources/data.csv", help="dataset_train.csv")
+    args = parser.parse_args()
+    df = pd.read_csv(args.path).drop(columns=['ID'])
+    df['Diagnosis'] = df['Diagnosis'] == 'M'
+    # normalized data
+    x_train, x_test, y_train, y_test = test_train_split(df.head(100), 0.8)
 
-# if __name__ == '__main__':
-#     main()
+    x_train=(x_train-x_train.mean())/x_train.std()
+    x_train = x_train.to_numpy(dtype=np.float64)
+    y_train = y_train.to_numpy(dtype=np.float64)
+    train(x_train, y_train)
+
+def train(x_train, y_train):
+    epochs = 1_000
+    x_len = x_train.shape[1]
+    layers = [
+        Layer(x_len, x_len, layer_type="input_layer", activation_function=relu),
+        Layer(x_len, x_len, layer_type="hidden_layer", activation_function=relu),
+        Layer(x_len, x_len, layer_type="hidden_layer", activation_function=relu),
+        Layer(x_len, 1, layer_type="output_layer")
+    ]
+
+    mp = MultilayerPerceptron(layers)
+    losses = []
+    for epoch in range(epochs):
+        print(x_train.shape, y_train.shape)
+        p = mp.backward_propagation(x_train.T, np.array(y_train).T)
+        loss = mp.loss(p, y_train)
+        losses.append(loss)
+        # print(f"epoch {epoch}/{epochs} - loss: {loss} - val_loss: {loss}")
+
+    pd.DataFrame(losses).plot()
+    plt.show()
+
+if __name__ == '__main__':
+    main()
+
